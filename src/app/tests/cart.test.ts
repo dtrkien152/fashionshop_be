df62da2c16c353ec17663e0efbb05bc23a5df1fd
duy300242 <duy300242@gmail.com>
2025-04-28 20:52:18 +0700

    update test unit

--- src/app/tests/cart.test.ts
+++ src/app/tests/cart.test.ts
@@ -24,6 +24,7 @@ describe('CartController - getCartForGuest', () => {
   });
 });
 
+
 describe('CartController - getCart', () => {
   const mockReq = { query: { fingerprint: 'e8d37bd4968c4d1f9c54ad16e0de8e7b' } } as unknown as Request;
   const mockRes = {
@@ -43,3 +44,45 @@ describe('CartController - getCart', () => {
     expect(mockNext).not.toHaveBeenCalled();
   });
 });
+
+describe('CartController - getCartForGuest:200', () => {
+  const mockReq = { query: { fingerprint: '8851271c-4ed4-4d9d-9a45-69df619c6889' } } as unknown as Request;
+  const mockRes = {
+    json: jest.fn(),
+    status: jest.fn().mockReturnThis(),
+  } as unknown as Response;
+
+  const mockNext = jest.fn() as NextFunction;
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Status success', async () => {
+    await controller.getCartForGuest(mockReq, mockRes, mockNext);
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+    expect(mockNext).not.toHaveBeenCalled();
+  });
+});
+
+
+describe('CartController - getCart By Token', () => {
+  const mockReq = {} as unknown as Request;
+  const mockRes = {
+    json: jest.fn(),
+    status: jest.fn().mockReturnThis(),
+  } as unknown as Response;
+
+  const mockNext = jest.fn() as NextFunction;
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Status success', async () => {
+    await controller.getCartForGuest(mockReq, mockRes, mockNext);
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+    expect(mockNext).not.toHaveBeenCalled();
+  });
+});
+
--- src/app/tests/category.test.ts
+++ src/app/tests/category.test.ts
@@ -1,10 +1,12 @@
 import { container } from '../config';
 import { NextFunction, Request, Response } from 'express';
 import CategoryController from '../controllers/category.controller';
+import { CategoryService } from '../services';
 
 const controller = container.resolve(CategoryController);
+const service = container.resolve(CategoryService);
 
-describe('CategoryController - getAllCategory', () => {
+describe('CategoryController - getAllCategory - Success', () => {
   const mockReq = {} as unknown as Request;
   const mockRes = {
     json: jest.fn(),
@@ -24,7 +26,7 @@ describe('CategoryController - getAllCategory', () => {
   });
 });
 
-describe('CategoryController - searchCategoriesForAdmin', () => {
+describe('CategoryController - searchCategoriesForAdmin - Success', () => {
   const mockReq = {} as unknown as Request;
   const mockRes = {
     json: jest.fn(),
@@ -41,5 +43,68 @@ describe('CategoryController - searchCategoriesForAdmin', () => {
     await controller.getAllCategory(mockReq, mockRes, mockNext);
     expect(mockRes.status).toHaveBeenCalledWith(200);
     expect(mockNext).not.toHaveBeenCalled();
+  });
+});
+
+describe('CategoryController - searchCategoriesForAdmin - Error', () => {
+  const mockReq = {} as unknown as Request;
+  const mockRes = {
+    json: jest.fn(),
+    status: jest.fn().mockReturnThis(),
+  } as unknown as Response;
+
+  const mockNext = jest.fn() as NextFunction;
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Status error', async () => {
+    await controller.searchCategoriesForAdmin(mockReq, mockRes, mockNext);
+    expect(mockNext).toHaveBeenCalled();
+  });
+});
+
+describe('CategoryService - getAllCategory - Not empty', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Status success', async () => {
+    const results = await service.getAllCategories();
+    expect(results.length).toBeGreaterThan(0);
+  });
+});
+
+describe('CategoryService - getCategoryById - Success', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Status success', async () => {
+    const results = await service.getCategoryById(0);
+    expect(results).resolves;
+  });
+});
+
+describe('CategoryService - getCategoryById - Not found', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Status error', async () => {
+    const results = await service.getCategoryById(-1);
+    expect(results).rejects;
+  });
+});
+
+describe('CategoryService - createCategory - Bad Request', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Status success', async () => {
+    const results = await service.createCategory(null);
+    expect(results).rejects;
   });
 });
