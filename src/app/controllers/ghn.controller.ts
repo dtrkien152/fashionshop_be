02a99adf4bd5644b8b213c3ca9df9654a4011c2b
nvggod <nvggod@gmail.com>
2025-05-12 22:23:50 +0700

    add filter data test province ghn

--- src/app/controllers/ghn.controller.ts
+++ src/app/controllers/ghn.controller.ts
@@ -12,7 +12,11 @@ class GHNController {
   getProvince = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
     try {
       const results = await this.ghnService.getProvince();
-      res.json(results.data.data);
+      if (!results.data.data || !results.data.data.length) {
+        res.json([]);
+      }
+      const dataValid = results.data.data.filter(el => ![2002, 298, 290, 286].includes(el.ProvinceID));
+      res.json(dataValid);
     } catch (error) {
       console.log(error);
       next(error);
@@ -21,8 +25,11 @@ class GHNController {
 
   getDistrict = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
     try {
-      const {provinceId}=req.query;
+      const { provinceId } = req.query;
       const results = await this.ghnService.getDistrict(Number(provinceId));
+      if (!results.data.data || !results.data.data.length) {
+        res.json([]);
+      }
       res.json(results.data.data);
     } catch (error) {
       next(error);
@@ -31,12 +38,16 @@ class GHNController {
 
   getWard = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
     try {
-      const {districtId}=req.query;
+      const { districtId } = req.query;
       const results = await this.ghnService.getWard(Number(districtId));
+      if (!results.data.data || !results.data.data.length) {
+        res.json([]);
+      }
       res.json(results.data.data);
     } catch (error) {
       next(error);
     }
   };
 }
+
 export default GHNController;
